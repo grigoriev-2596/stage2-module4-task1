19615b2c5580b163e28b037d40371c9c9dfbab65
Grigoriev Egor <grigoriev.egor2596@gmail.com>
2022-11-25 01:22:44 +0300

    some changes to singleton

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -2,21 +2,18 @@ package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
     private static volatile ThreadSafeSingleton instance;
-    private static Object mutex = new Object();
     private ThreadSafeSingleton() {
 
     }
 
     public static ThreadSafeSingleton getInstance() {
-        ThreadSafeSingleton localRef = instance;
-        if (localRef == null) {
-            synchronized (mutex) {
-                localRef = instance;
-                if (localRef == null) {
-                    localRef = instance = new ThreadSafeSingleton();
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
                 }
             }
         }
-        return localRef;
+        return instance;
     }
 }
